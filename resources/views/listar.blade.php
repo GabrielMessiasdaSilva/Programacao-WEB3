f75a05b51e686afcbdb3e706d3260a2c5970394b
GabrielMessiasdaSilva <gabrielestudante420@gmail.com>
2024-03-06 14:23:57 -0300

    laravell

---
+++ resources/views/listar.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Aula 2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="grid text">
+        <div class="container text">
+            <div class="row align-items-start">
+                <nav class="navbar bg-dark">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">
+                            <img src="https://th.bing.com/th/id/R.bdcb089ce860484bbc80aedeef452b34?rik=dTqlu3IljX45gw&pid=ImgRaw&r=0" alt="Logo" width="40" height="40" class="d-inline-block align-text-top">
+                         
+                        </a>
+                    </div>
+                </nav>
+
+                <P>
+                <h5>Cadastrar - Agendamento de Potencias Clientes</h5>
+                </P>
+                <br>
+                <h6>Sistema Utilizado para agendamento de serviços.</h6>
+                <br> <br> <br> <br>
+                <table class="table-responsive table table-primary table-striped ">
+                    <thead>
+                        <tr>
+                            <th class="text-bg-light p-3" scope="col">Nome</th>
+                            <th class="text-bg-light p-3" scope="col">endereço</th>
+                            <th class="text-bg-light p-3" scope="col">bairro</th>
+                            <th class="text-bg-light p-3" scope="col">cep</th>
+                            <th class="text-bg-light p-3" scope="col">cidade</th>
+                            <th class="text-bg-light p-3" scope="col">estado </th>
+                            <th class="text-bg-light p-3" scope="col">ação </th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
