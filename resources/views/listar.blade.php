30661bc3d319ec19e08da499a6969ee68851a914
GabrielMessiasdaSilva <gabrielestudante420@gmail.com>
2024-03-14 00:32:47 -0300

    atualizando

--- resources/views/listar.blade.php
+++ resources/views/listar.blade.php
@@ -1,49 +1,72 @@
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Aula 2</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>ADIDAS</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <link rel="shortcut icon" href="../imagens/logo.png" type="image/x-icon">
 </head>
 
 <body>
-    <div class="grid text">
-        <div class="container text">
-            <div class="row align-items-start">
-                <nav class="navbar bg-dark">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">
-                            <img src="https://th.bing.com/th/id/R.bdcb089ce860484bbc80aedeef452b34?rik=dTqlu3IljX45gw&pid=ImgRaw&r=0" alt="Logo" width="40" height="40" class="d-inline-block align-text-top">
-                         
-                        </a>
-                    </div>
-                </nav>
-
-                <P>
-                <h5>Cadastrar - Agendamento de Potencias Clientes</h5>
-                </P>
-                <br>
-                <h6>Sistema Utilizado para agendamento de serviços.</h6>
-                <br> <br> <br> <br>
-                <table class="table-responsive table table-primary table-striped ">
-                    <thead>
-                        <tr>
-                            <th class="text-bg-light p-3" scope="col">Nome</th>
-                            <th class="text-bg-light p-3" scope="col">endereço</th>
-                            <th class="text-bg-light p-3" scope="col">bairro</th>
-                            <th class="text-bg-light p-3" scope="col">cep</th>
-                            <th class="text-bg-light p-3" scope="col">cidade</th>
-                            <th class="text-bg-light p-3" scope="col">estado </th>
-                            <th class="text-bg-light p-3" scope="col">ação </th>
-                        </tr>
-                    </thead>
-                </table>
+  <div class="grid text">
+    <div class="container text">
+      <div class="row align-items-start">
+        <nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+       
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+              <img src="https://th.bing.com/th/id/R.5d4bee3b7e0bc80ba1ff8cfe64fec3e9?rik=yTG9Rg2sEAgNrw&riu=http%3a%2f%2ficons.iconseeker.com%2fpng%2f128%2ffootball-marks%2fadidas-white-logo.png&ehk=CeWLhLjUMvEA3UsvoanMrJ5Rq%2fhlRmzEpsRoD0Gsvhg%3d&risl=&pid=ImgRaw&r=0" alt="Logo" width="50" height="45" class="d-inline-block align-text-top">
+            
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Produtos</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Lojas</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled">Contato</a>
+                </li>
+              </ul>
             </div>
+          </div>
+        </nav>
+        <p>
+        <h5 >Cadastro de Novos Produtos - ADIDAS</h5>
+        </P>
+        <br>
+        <p >Sistema Utilizado para Cadastro de Novos Produtos Adidas</p>
+        
+        <br> <br> <br> <br>
+        <div class="table-responsive">
+          <table class="table">
+            <thead>
+              <tr>
+                <th class="text-bg-light p-3" scope="col">Nome</th>
+                <th class="text-bg-light p-3" scope="col">endereço</th>
+                <th class="text-bg-light p-3" scope="col">bairro</th>
+                <th class="text-bg-light p-3" scope="col">cep</th>
+                <th class="text-bg-light p-3" scope="col">cidade</th>
+                <th class="text-bg-light p-3" scope="col">estado </th>
+                <th class="text-bg-light p-3" scope="col">ação </th>
+              </tr>
+            </thead>
+          </table>
         </div>
+      </div>
     </div>
+  </div>
 
 </body>
 
